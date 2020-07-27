7489de5ef23bc35f48aeb4757416f1076a389ffa
nkink <namseok.kang@gmail.com>
2020-07-27 03:55:56 +0100

    final

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session
+from flask import Flask, render_template, session, request
 app = Flask(__name__)
 app.secret_key = 'app secret key'
 
@@ -6,21 +6,23 @@ app.secret_key = 'app secret key'
 def home():
     return render_template("index.html")
 
-@app.route("/print")
+@app.route("/print", methods=['GET'])
 def print():
     from brother import BrotherPrint
     import socket
+    import json
+    from datetime import datetime
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect(('192.168.1.143',9100))
         printjob = BrotherPrint(s)
         printjob.template_mode()
         printjob.template_init()
-        printjob.select_and_insert("id", "dall")
-        printjob.select_and_insert("date", "2020/08/30")
-        printjob.select_and_insert("reserved1", "O")
-        printjob.select_and_insert("reserved2", "O")
-        printjob.select_and_insert("reserved3", "O")
+        printjob.select_and_insert("id", request.args.get('login','F'))
+        printjob.select_and_insert("date", datetime.today().strftime("%Y/%m/%d"))
+        printjob.select_and_insert("reserved1", request.args.get('9','F'))
+        printjob.select_and_insert("reserved2", request.args.get('14','F'))
+        printjob.select_and_insert("reserved3", request.args.get('19','F'))
         printjob.template_print()
     return ('printed')
         
